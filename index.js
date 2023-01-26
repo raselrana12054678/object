4e678fb6f5fd087f0f731f901732dcabeffbd057
Rasel Rana <107293742+raselrana12054678@users.noreply.github.com>
2023-01-26 20:42:17 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var name = "anisul  islam";
+var age = 27;
+var Gpa = 3.45;
+var lang = ["bangali", "english", ]
+
+var student1 = {
+    name: "rasel",
+    age : 25,
+    gpa : 3.93,
+    lang : ["bhangali", "Hindi", "English"]
+}
+document.write(lang);
